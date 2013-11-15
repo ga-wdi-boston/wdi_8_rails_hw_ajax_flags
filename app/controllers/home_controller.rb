fd6672b8beb0c231223d2d82e4155eaea791a687
ChrisSherlin <rhymeswithmerlin@gmail.com>
2013-11-15 18:30:51 -0500

    commented out infinite scrolling. Going to merge this branch to master

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,13 +1,13 @@
 class HomeController < ApplicationController
   def index
-    # @countries = Country.all
-        @countries = Country.paginate(:page => params[:page]) # replaces Post.all
-    respond_to do |format|
-      format.html
-      format.js # add this line for your js template
+     @countries = Country.all
+       # @countries = Country.paginate(:page => params[:page]) # replaces Post.all
+    # respond_to do |format|
+    #   format.html
+    #   format.js # add this line for your js template
     end
   end
 
   def countries
   end
-end
+
---
+++ app/views/home/countries.js
@@ -0,0 +1,5 @@
+// $('.countries').append('#{escape_javascript(render partial: "shared/countries", :locals => { :countries => @countries })}');
+// $('a.load-more-countries').attr('href', '#{countries_path page: @countries.next_page}');
+ 
+// unless @countries.next_page
+//   $('a.load-more-countries').remove();
