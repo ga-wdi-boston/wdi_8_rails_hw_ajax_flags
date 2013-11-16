9bab56dcef7eecc41c8b3fc265d78f7ee7dbd837
Ryan Page <rpage326@gmail.com>
2013-11-16 16:21:43 -0500

    first commit

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def index
+  	@countries = Country.all
   end
 
   def countries
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 AjaxFlags::Application.routes.draw do
   root :to => "home#index"
+  resources :home
   get "/countries/:step/:offset" => "home#countries"
   get "/country/:id" => "home#country"
 end
+
