84ddf70d592d5e04e05911efb16f8207a5d41183
Abby Howell <abby.g.howell@gmail.com>
2013-11-17 16:09:09 -0500

    trying everything, still stuck

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -8,41 +8,33 @@ var Flags = {
     $('#flags').empty();
 	},
 	showMoreCountries: function() {
-		$('#flags').load('/countries');
+		$.ajax({
+			url: $('.pagination .next_page').attr('href'),
+			dataType: 'script',
+			success: function(data) {
+				console.log(data);
+			}
 	},
 
-	element_in_scroll: function(elem) {
-    var docViewTop = $(window).scrollTop();
-    var docViewBottom = docViewTop + $(window).height();
- 
-    var elemTop = $(elem).offset().top;
-    var elemBottom = elemTop + $(elem).height();
- 
-    return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
-	},
 	infiniteScroll: function() {
-		if (Flags.element_in_scroll('#flags div:last')) {
-      $(document).unbind('scroll');
-	      $.ajax({
-	          type: 'GET',
-            url: document.location.href,
-            data: { text_filter:  $('#text_filter').attr('value'), index_count:$('#index_count').attr('value'),json: "true" }
-        }).done(function( data ) {
-	          $('#flags').append(data.html);
-	          });
-    // if ( $(window).scrollTop() > $(document).height() - $(window).height() - 50 ) {
-    // 	$('#flags').load('/countries');
-    // }
-    }
+
+ //  if ( $(window).scrollTop() > $(document).height() - $(window).height() - 50 ) {
+ //  	$('#flags').load($('.pagination .next_page').attr('href'));
+ //  }
+
 	}
 
 
-}
+};
 
 $(document).ready(function() {
 	$('#populate-all-button').click(Flags.populateAllCountries);
 	$('#reset-button').click(Flags.resetAllCountries);
 	$('#show-more-countries-button').click(Flags.showMoreCountries);
+	$('.pagination .next_page').click(function(e){
+     e.preventDefault();
+     Flags.showMoreCountries();
+     });
 	$(window).scroll(Flags.infiniteScroll);
 });
 
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,22 +1,23 @@
 class HomeController < ApplicationController
-	# respond_to :json, :js, :html
+	respond_to :json, :js, :html
 
   def index
     @countries = Country.paginate(:page => params[:page])
+    render file: index.js.erb 
   end
 
   # def countries
   #   @countries = Country.paginate(:page => params[:page])
   # end
 
-  def country
-    # @country = Country.where(params[:id])
-  end
+  # def country
+  #   # @country = Country.where(params[:id])
+  # end
 
-  def all
-    @countries = Country.all
-    respond_with @countries
-  end
+  # def all
+  #   @countries = Country.all
+  #   respond_with @countries
+  # end
 
 
 end
