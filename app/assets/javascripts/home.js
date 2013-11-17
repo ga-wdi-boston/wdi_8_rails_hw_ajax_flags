60117c606dbdb2cdf4a670bc89c945652f90a134
ChrisSherlin <rhymeswithmerlin@gmail.com>
2013-11-17 14:51:50 -0500

    trying to get the a new page to load each time. created logic in the home.js file, but I think it is misplaced, because it is erroring out

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -14,6 +14,7 @@ var Flags = {
 	infiniteScroll: function() {
 		var flagsLoaded = $('.f32').length;
 		var flagsTotal  = $('.countries-container').data('total-countries'); // the data function reads data attributes out of the DOM
+
 		$(window).scroll(function(){  
 			if ($(window).scrollTop() == $(document).height() - $(window).height() && flagsLoaded < flagsTotal && !Flags.isLoading) { //scrollTop == how far you are scrolled down; doc height is the height of the page rendered at once. window height is the height ofthe window
 				console.log('douche');
@@ -21,7 +22,17 @@ var Flags = {
 				$.ajax({
 					url: "", //my partial 
 					data: { 
-						start: flagsLoaded //hey start at flag twenty one dumbass
+						start: { 
+							if (flagsLoaded === flagsTotal) {
+								alert('done')
+							} else {
+								i = 0;
+								flagsLoaded + i;
+								i += 20
+							}
+						;}  //hey start at flag twenty one dumbass. This is what to change, i think
+
+						
 					}
 				}).done(function(data){
 						$('.countries-container').append(data);
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,8 @@
 class HomeController < ApplicationController
+    respond_to :json, :js, :html
   def index
      @countries = Country.all
-       # @countries = Country.paginate(:page => params[:page]) # replaces Post.all
+        #@countries = Country.paginate(:page => params[:page]) # replaces Post.all
     # respond_to do |format|
     #   format.html
     #   format.js # add this line for your js template
