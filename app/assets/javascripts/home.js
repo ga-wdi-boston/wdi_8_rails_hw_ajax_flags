fbade7d52d6a7a851f90385cb5bbf5cce146ec17
ChrisSherlin <rhymeswithmerlin@gmail.com>
2013-11-17 00:58:55 -0500

    progressing through infinite scroll

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -9,13 +9,19 @@ var Flags = {
 
 	},
 	infiniteScroll: function() {
+		var flagCount = $('.f32').length;
 		$(window).scroll(function(){  
-			if  ($(window).scrollTop() == $(document).height() - $(window).height()){ 
-				loadArticle(count);  
-				count++;  
+			if  ($(window).scrollTop() == $(document).height() - $(window).height()){ //scrollTop == how far you are scrolled down; doc height is the height of the page rendered at once. window height is the height ofthe window
+				$.ajax({
+					url: "", //my partial 
+					data: { 
+						start: flagCount //hey start at flag twenty one dumbass
+					}
+				})
 			}  
 		});    
-	});
+	}
+};
 
 $(document).ready(function() {
 	$('#populate-all-button').click(Flags.populateAllCountries);
