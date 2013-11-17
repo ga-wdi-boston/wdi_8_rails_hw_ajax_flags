260f252a30519f114ed9a35c2869703dc5fe7115
ChrisSherlin <rhymeswithmerlin@gmail.com>
2013-11-17 16:20:16 -0500

    got a working country list

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -15,31 +15,20 @@ var Flags = {
 		var flagsLoaded = $('.f32').length;
 		var flagsTotal  = $('.countries-container').data('total-countries'); // the data function reads data attributes out of the DOM
 
-		$(window).scroll(function(){  
-			if ($(window).scrollTop() == $(document).height() - $(window).height() && flagsLoaded < flagsTotal && !Flags.isLoading) { //scrollTop == how far you are scrolled down; doc height is the height of the page rendered at once. window height is the height ofthe window
-				console.log('douche');
-				Flags.isLoading = true;
-				$.ajax({
-					url: "", //my partial 
-					data: { 
-						start: { 
-							if (flagsLoaded === flagsTotal) {
-								alert('done')
-							} else {
-								i = 0;
-								flagsLoaded + i;
-								i += 20
-							}
-						;}  //hey start at flag twenty one dumbass. This is what to change, i think
 
-						
-					}
-				}).done(function(data){
-						$('.countries-container').append(data);
-						Flags.isLoading = false;
-				}); //this is called when you are done loading data
-			}  
-		});    
+		if ($(window).scrollTop() == $(document).height() - $(window).height() && flagsLoaded < flagsTotal && !Flags.isLoading) { //scrollTop == how far you are scrolled down; doc height is the height of the page rendered at once. window height is the height ofthe window
+			console.log(flagsLoaded);
+			Flags.isLoading = true;
+			$.ajax({
+				url: "", //my partial 
+				data: { 
+					start: flagsLoaded   //hey start at flag twenty one dumbass. This is what to change, i thin
+				}
+			}).done(function(data){
+					$('.countries-container').append(data);
+					Flags.isLoading = false;
+			}); //this is called when you are done loading data
+		}   
 	}
 };
 
