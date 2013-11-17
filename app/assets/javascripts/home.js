664f25c211b3488920b61f424ea5685875438bc5
ChrisSherlin <rhymeswithmerlin@gmail.com>
2013-11-17 16:44:32 -0500

    fixed spacing, and cleaned up logic.

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -11,16 +11,17 @@ var Flags = {
 	showMoreCountries: function() {
 
 	},
-	infiniteScroll: function() {
+	infiniteScroll: function() { 
+		var atBottom = $(window).scrollTop() == $(document).height() - $(window).height();
 		var flagsLoaded = $('.f32').length;
 		var flagsTotal  = $('.countries-container').data('total-countries'); // the data function reads data attributes out of the DOM
 
+		if (atBottom && flagsLoaded < flagsTotal && !Flags.isLoading) { //scrollTop == how far you are scrolled down; doc height is the height of the page rendered at once. window height is the height ofthe window
 
-		if ($(window).scrollTop() == $(document).height() - $(window).height() && flagsLoaded < flagsTotal && !Flags.isLoading) { //scrollTop == how far you are scrolled down; doc height is the height of the page rendered at once. window height is the height ofthe window
-			console.log(flagsLoaded);
 			Flags.isLoading = true;
+
 			$.ajax({
-				url: "", //my partial 
+				url: "/", //my partial 
 				data: { 
 					start: flagsLoaded   //hey start at flag twenty one dumbass. This is what to change, i thin
 				}
@@ -28,14 +29,16 @@ var Flags = {
 					$('.countries-container').append(data);
 					Flags.isLoading = false;
 			}); //this is called when you are done loading data
+
 		}   
+	},
+	init: function() { //nested anon function as an event handler
+		$('#populate-all-button').click(Flags.populateAllCountries);
+		$('#reset-button').click(Flags.resetAllCountries); //my comments are strange
+		$('#show-more-countries-button').click(Flags.showMoreCountries); //the sexy $('') thing is the selector
+		$(window).scroll(Flags.infiniteScroll); //scroll binding. and event handler is a function that is called when an event fires
 	}
 };
 
-$(document).ready(function() {
-	$('#populate-all-button').click(Flags.populateAllCountries);
-	$('#reset-button').click(Flags.resetAllCountries);
-	$('#show-more-countries-button').click(Flags.showMoreCountries);
-	$(window).scroll(Flags.infiniteScroll);
-});
+$(document).ready(Flags.init);
 
