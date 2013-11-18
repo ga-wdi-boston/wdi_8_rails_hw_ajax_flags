b968956d660f23f929bf8aa8ea248a669d2d2502
Cori Allen <coriallen@gmail.com>
2013-11-17 21:04:33 -0500

    Follow amatsudas kaminari example to ajaxify pagination links, render partial to clean up index erb view.

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -14,7 +14,10 @@ var Flags = {
 };
 
 $(document).ready(function() {
-	alert("dom is loaded!");
+	// $('.first a').click(function {
+	// 	$.get(this.href, null, null, "script");
+	// 	return false;
+	// });
 	$('#populate-all-button').click(Flags.populateAllCountries);
 	$('#reset-button').click(Flags.resetAllCountries);
 	$('#show-more-countries-button').click(Flags.showMoreCountries);
