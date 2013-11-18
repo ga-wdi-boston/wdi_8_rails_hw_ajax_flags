738be08bb58aba4fe5fce7b23228adf968935f99
Cori Allen <coriallen@gmail.com>
2013-11-17 22:10:03 -0500

    Following railscast #114, create infinite scroll function with reference to countries.js.

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -9,15 +9,33 @@ var Flags = {
 
 	},
 	infiniteScroll: function() {
+		var currentPage = 1;
 
-	}
+		function checkScroll() {
+		  if (nearBottomOfPage()) {
+		    currentPage++;
+		    new Ajax.Request('/countries.js?page=' + currentPage, {asynchronous:true, evalScripts:true, method:'get'});
+		  } else {
+		    setTimeout("checkScroll()", 250);
+		  }
+		}
+
+		function nearBottomOfPage() {
+		  return scrollDistanceFromBottom() < 150;
+		}
+
+		function scrollDistanceFromBottom(argument) {
+		  return pageHeight() - (window.pageYOffset + self.innerHeight);
+		}
+
+		function pageHeight() {
+		  return Math.max(document.body.scrollHeight, document.body.offsetHeight);
+		}
+
+		document.observe('dom:loaded', checkScroll);
 };
 
 $(document).ready(function() {
-	// $('.first a').click(function {
-	// 	$.get(this.href, null, null, "script");
-	// 	return false;
-	// });
 	$('#populate-all-button').click(Flags.populateAllCountries);
 	$('#reset-button').click(Flags.resetAllCountries);
 	$('#show-more-countries-button').click(Flags.showMoreCountries);
