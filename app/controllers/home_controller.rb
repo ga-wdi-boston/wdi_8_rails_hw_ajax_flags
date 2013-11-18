b5bd88925e67d725e84dd2149d3e6cd0c31381c7
brycelambert <brycelambert@gmail.com>
2013-11-18 10:42:50 -0500

    deleted lines

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,18 +7,6 @@ class HomeController < ApplicationController
     end
   end
 
-#   def load
-#     @start_point = params[:start].to_i
-#     @countries = Country.where('id < ?', @start_point).limit(20)
-#     respond_to do |format|
-#       format.js
-#     end
-#   end
-
-# end
-
-
-
   def load
     @countries = Country.limit(20).offset(params[:offset])
     respond_to do |format|
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 AjaxFlags::Application.routes.draw do
   root :to => "home#index"
-  get "/countries/:step/:offset" => "home#countries"
-  get "/country/:id" => "home#country"
+  # get "/countries/:step/:offset" => "home#countries"
+  # get "/country/:id" => "home#country"
 
   get "/load" => "home#load"
   
