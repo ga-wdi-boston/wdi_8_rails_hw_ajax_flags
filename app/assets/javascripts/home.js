32e6ef5a0cdac8cf3fa43882784a197a9209f9bd
K Mak <karen.mak@stx.oxon.org>
2014-02-27 18:04:25 -0500

    can get a list of countries with abbr with button

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,7 +1,15 @@
 var Flags = {
-	populateAllCountries: function() {
-
+	populateAllCountries: function(){
+		$.ajax({
+			url: '/countries',
+			type: 'get',
+			dataType: 'json'
+		})
+		.done(function(data){
+			FlagApp.renderAllCountries(data);
+		});
 	},
+
 	resetAllCountries: function() {
 
 	},
@@ -13,9 +21,24 @@ var Flags = {
 	}
 };
 
+var FlagApp = FlagApp || {}
+
+FlagApp.renderAllCountries = function(countries) {
+	var numCountries = countries.length, i = 0;
+		for(; 1 < numCountries; i++) {
+			FlagApp.renderCountry(countries[i]);
+		}
+};
+
+FlagApp.renderCountry = function(country) {
+	var $countryDiv = $('<div>'+ country.name + ' (' + country.abbreviation + ') ' + '</div>' );
+	this.$countriesDiv.append($countryDiv);
+}
+
 $(document).ready(function() {
 	$('#populate-all-button').click(Flags.populateAllCountries);
 	$('#reset-button').click(Flags.resetAllCountries);
 	$('#show-more-countries-button').click(Flags.showMoreCountries);
 	$(window).scroll(Flags.infiniteScroll);
+	FlagApp.$countriesDiv = $('#country-list');
 });
