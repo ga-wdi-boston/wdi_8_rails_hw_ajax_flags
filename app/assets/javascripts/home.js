9443f852bcdcd7cdc7fffc1cecd4c8f94de76441
Yiyang Song <songyiyang2012@gmail.com>
2014-02-27 21:03:36 -0500

    homework

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,21 +1,51 @@
 var Flags = {
 	populateAllCountries: function() {
-
 	},
 	resetAllCountries: function() {
-
+		$(".f32").empty();
 	},
 	showMoreCountries: function() {
-
+		$.ajax({
+		url: '/',
+		type: 'get',
+		dataType: 'json'
+		})
+		.done(function(data){
+			var length = $('.f32').children().size();
+			for(var i=0; i<10; i++){
+				$('.f32').append('<li '+ "class='flag " + data[length+i].abbreviation + "'>" + '</li>');
+			}
+		});
 	},
 	infiniteScroll: function() {
 
+
+if ($(window).scrollTop() >= $(document).height() - $(window).height() + 4) {
+$.ajax({
+					url: '/',
+					type: 'get',
+					dataType: 'json'
+					})
+					.done(function(data){
+						var length = $('.f32').children().size();
+						for(var i=0; i<10; i++){
+							$('.f32').append('<li '+ "class='flag " + data[length+i].abbreviation + "'>" + '</li>');
+						}
+						Flags.count += 1;
+					});
+    }
+
+
+			
+		
 	}
 };
 
 $(document).ready(function() {
+	Flags.count = 1;
 	$('#populate-all-button').click(Flags.populateAllCountries);
 	$('#reset-button').click(Flags.resetAllCountries);
 	$('#show-more-countries-button').click(Flags.showMoreCountries);
 	$(window).scroll(Flags.infiniteScroll);
+	
 });
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,9 @@
 class HomeController < ApplicationController
   def index
+  	@countries = Country.all
+  	respond_to do |f|
+			f.html # does default without a block (index.html.erb)
+			f.json { render json: @countries}
+		end
   end
 end
