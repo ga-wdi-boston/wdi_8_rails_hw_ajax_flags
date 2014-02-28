0757b54c241cc0bbd2239f04c42dc6e7adf0e47a
Praggya Rustagi Barretto <praggya@gmail.com>
2014-02-28 01:26:01 -0500

    refactored show all and reset and added infinite scroll and removed country partial and made north american countries outlined in tomato

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,26 +1,87 @@
 var Flags = {
 	populateAllCountries: function() {
-		$('.f32').show();
-	},
-	resetAllCountries: function() {
-		$('.f32').hide();
-	},
-	showMoreCountries: function() {
-		Flags.resetAllCountries();
-		var i = 5;
-		for (; i < 25;) {
-			$('.f32:nth-child(' + i + ')').show();
-			i += 1;
-		}
-	},
-	infiniteScroll: function() {
+		$flagsDiv = $('#all-countries')
 
+		$.ajax({
+				url: '/countries',
+				type: 'GET',
+				dataType: 'json'
+			})
+			.done(function(data) {
+				Flags.renderCountries(data);
+				console.log('all flags loaded')
+			})
+			.fail(function() {
+				console.log("error");
+			})
+			.always(function() {
+				console.log("complete");
+			});
+	},
+
+	resetAllCountries: function() {
+		$('#all-countries').empty()
+	},
+
+	showMoreCountries: function(start, limit) {
+		$.ajax({
+			url: '/countries',
+			type: 'GET',
+			dataType: 'json',
+			data: {offset: start, limit: limit}
+ 			})
+ 			.done(function(data) {
+ 				Flags.renderCountries(data);
+ 			})
+ 			.fail(function() {
+				console.log("error");
+			})
+	},
+
+	infiniteScroll: function() {
+		Flags.showMoreCountries(Flags.display_countries, 20);
 	}
 };
 
 $(document).ready(function() {
-	$('#show-all-button').click(Flags.populateAllCountries);
+	Flags.display_countries = 0;
+
+	$('#populate-all-button').click(Flags.populateAllCountries);
 	$('#reset-button').click(Flags.resetAllCountries);
-	$('#show-twenty-button').click(Flags.showMoreCountries);
-	$(window).scroll(Flags.infiniteScroll);
+	$('#show-more-countries-button').click(function(event) {
+	 	event.preventDefault();
+	 	Flags.showMoreCountries(Flags.display_countries, 20);
+	 	return false;
+	});
+
+	$(window).scroll(function(){
+		if($(document).height()==$(window).scrollTop()+$(window).height()){
+			Flags.infiniteScroll();
+			}
+		});
+
 });
+
+
+Flags.renderCountries = function(countries) {
+	var numFlags = countries.length;
+	for (var i = 0; i < numFlags; i++) {
+		Flags.renderCountry(countries[i]);
+		Flags.display_countries += 1;
+	};
+};
+
+Flags.renderCountry = function(country) {
+	var $all_countries = $('#all-countries'),
+	 		$country_div = $('<div id="country_' + country.id +'" class="f32">'),
+	 		$country_flag = $('<div class="flag ' + country.abbreviation + '">'),
+	 		$country_info = $('<div>', { id: 'country_' + country.id, class: 'country', html: country.name});
+
+	 		if (country.north_america) {
+	 			$country_div.addClass('north-america');
+			}
+
+
+	 		$country_div.append($country_flag, $country_info);
+	 		$all_countries.append($country_div);
+}
--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -1,5 +1,14 @@
 class CountriesController < ApplicationController
   def index
+    if params[:limit]
+      @countries = Country.find(:all, offset: params[:offset], limit: params[:limit])
+    else
+      @countries = Country.all
+    end
 
+    respond_to do |format|
+      format.html
+      format.json { render json: @countries }
+    end
   end
 end
