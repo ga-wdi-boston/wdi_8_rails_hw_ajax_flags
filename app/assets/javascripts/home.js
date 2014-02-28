316df8d4daf23f3463451a3b4281ab2a2fbec0f5
emreiser <emilie.reiser@gmail.com>
2014-02-28 06:26:51 -0500

    Get all countries and render country functions complete.

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,21 +1,74 @@
 var Flags = {
-	populateAllCountries: function() {
+	offset: 0,
 
-	},
-	resetAllCountries: function() {
+	getAllCountries: function() {
+		$.ajax({
+			url: '/countries',
+			type: 'GET',
+			dataType: 'json',
+			data: {offset: 0}
+		})
 
+		.done(function(data) {
+			console.log("success");
+			Flags.renderCountries(data);
+			Flags.offset += data.length;
+		})
+
+		.fail(function() {
+			console.log("error");
+		})
+
+		.always(function() {
+			console.log("complete");
+		});
 	},
+
 	showMoreCountries: function() {
 
+
 	},
+
+	clearAllCountries: function() {
+		Flags.countriesUl.empty();
+		Flags.offset = 0;
+
+	},
+
 	infiniteScroll: function() {
+
+	},
+
+	renderCountry: function(country){
+		var $countryLi = $('<li />', {class: 'panel panel-default'}),
+				$title = $('<h3 />', {html: country.name + ' (' + country.abbreviation.toUpperCase() + ')'}),
+				$imgDiv = $('<div />', {class: 'flag ' + country.abbreviation});
+
+		$countryLi.append($title);
+		$countryLi.append($imgDiv);
+		if(country.north_america === true){
+			$countryLi.addClass('n-a');
+		}
+
+		return $countryLi;
+	},
+
+	renderCountries: function(countries){
+		var i = 0, length = countries.length;
+
+		for(;i < length;){
+			var $country = Flags.renderCountry(countries[i]);
+			Flags.countriesUl.append($country);
+			i = i + 1;
+		};
 
 	}
 };
 
 $(document).ready(function() {
-	$('#populate-all-button').click(Flags.populateAllCountries);
-	$('#reset-button').click(Flags.resetAllCountries);
+	Flags.countriesUl = $('#all-countries');
+	$('#populate-all-button').click(Flags.getAllCountries);
+	$('#clear-button').click(Flags.clearAllCountries);
 	$('#show-more-countries-button').click(Flags.showMoreCountries);
-	$(window).scroll(Flags.infiniteScroll);
+	$(window).scroll(Flags.showMoreCountries);
 });
--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -1,4 +1,12 @@
 class CountriesController < ApplicationController
   def index
+  	@countries = Country.all
+  end
+
+  	respond_to do |format|
+  		format.html
+  		format.json { render json: @countries }
+
+  	end
   end
 end
