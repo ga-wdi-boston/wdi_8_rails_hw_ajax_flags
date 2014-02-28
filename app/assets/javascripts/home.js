02e8b1d3a0b663c49d8b9b7a2f54b9ecded3fbe0
nevermore2014 <zhou.hongyu8976@gmail.com>
2014-02-27 23:08:36 -0500

    Can populate all the countries and reset now

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,9 +1,22 @@
 var Flags = {
 	populateAllCountries: function() {
+		$.ajax({
+			url: '/countries',
+			type: 'GET',
+			dataType: 'json'
+		}).done(function(data){
+			Flags.renderCountries(data);
+		});
 
 	},
 	resetAllCountries: function() {
-
+		$.ajax({
+			url: '/countries',
+			type: 'GET',
+			dataType: 'json'
+		}).done(function(data){
+			Flags.emptyCountries(data);
+		});
 	},
 	showMoreCountries: function() {
 
@@ -14,8 +27,26 @@ var Flags = {
 };
 
 $(document).ready(function() {
+	Flags.$countriesDiv = $('#all_countries');
+
 	$('#populate-all-button').click(Flags.populateAllCountries);
 	$('#reset-button').click(Flags.resetAllCountries);
 	$('#show-more-countries-button').click(Flags.showMoreCountries);
 	$(window).scroll(Flags.infiniteScroll);
 });
+
+Flags.renderCountries = function(countries){
+	var numCountries = countries.length, i = 0;
+	for(i; i < numCountries; i++){
+		Flags.renderCountry(countries[i]);
+	}
+}
+
+Flags.renderCountry = function(country){
+	var $countryDiv = $('<div>', {text: country.name});
+	this.$countriesDiv.append($countryDiv);
+}
+
+Flags.emptyCountries = function(country){
+	this.$countriesDiv.empty();
+}
