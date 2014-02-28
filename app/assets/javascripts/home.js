f5569aaad4dad171b4e905214b6dbec6048338fe
Laure Fischer <laurefischer27@gmail.com>
2014-02-27 22:13:39 -0500

    all countries outputted to page when populate all button is clicked

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,25 +1,53 @@
-var Flags = {
-	populateAllCountries: function() {
-
-	},
-	resetAllCountries: function() {
-
-	},
-	showMoreCountries: function() {
-
-	},
-	infiniteScroll: function() {
-		var win = $(window);
-    // Infinite scroll math!
-    if(win.height() + win.scrollTop() >= $(document).height()) {
-      showMoreCountries();
-    }
-	}
-};
-
 $(document).ready(function() {
+	Flags.$countriesDiv = $('#all-countries');
 	$('#populate-all-button').click(Flags.populateAllCountries);
 	$('#reset-button').click(Flags.resetAllCountries);
 	$('#show-more-countries-button').click(Flags.showMoreCountries);
 	$(window).scroll(Flags.infiniteScroll);
 });
+
+var Flags = Flags || {};
+
+Flags.populateAllCountries = function(countries) {
+
+	$.ajax({
+		url: '/countries',
+		type: 'GET',
+		dataType: 'json'
+	})
+	.done(function(data) {
+		var numCountries = data.length, i = 0;
+		for(i; i < numCountries; i++) {
+	  	Flags.renderCountry(data[i]);
+		};
+	})
+	.fail(function() {
+		console.log("error");
+	})
+};
+
+Flags.renderCountry = function(country) {
+	if(country !== undefined) {
+		var countryDiv = '<div data-bool= ' + country.north_america  + ' id='+ country.id +'>';
+			  countryDiv += ' Name: ' + country.name + ' Abbreviation: ' + country.abbreviation;
+			  countryDiv += '<span class="flag ' + country.abbreviation + '"></span>'
+			  countryDiv += '</div>'
+		Flags.$countriesDiv.append(countryDiv);
+	};
+};
+
+Flags.resetAllCountries = function() {
+
+};
+
+Flags.showMoreCountries = function() {
+
+};
+
+Flags.infiniteScroll = function() {
+	var win = $(window);
+  // Infinite scroll math!
+  if(win.height() + win.scrollTop() >= $(document).height()) {
+    showMoreCountries();
+  };
+};
